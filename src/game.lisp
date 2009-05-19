cb2e18717f0b484b9bd59526b6cd396496ba8559
Kat Marchán <kzm@sykosomatic.org>
2009-05-18 20:58:09 -0400

    Compiles again

--- src/game.lisp
+++ src/game.lisp
@@ -19,8 +19,8 @@
 (defbuzzword load-game (engine filename))
 (defmessage load-game ((engine =engine=) filename)
   "Loads a game script and returns the configured GAME object."
-  (declare (ignore engine))
-  (let ((*engine* (clone (=engine=)
+  (declare (ignore engine filename))
+  #+nil(let ((*engine* (clone (=engine=)
 		       ((current-game (clone (=game=) ()))))))
     (load (merge-pathnames (concatenate 'string name ".lisp") (get-game-path)))
     (setf (background (current-game *engine*)) (clone (=background=) ()))
@@ -34,22 +34,17 @@
 
 ;;; Messages
 (defmessage init ((game =game=))
+  (declare (ignore game))
   ;; texture initialization goes here
   )
 (defmessage teardown ((game =game=))
+  (declare (ignore game))
   ;; texture deletion goes here
   )
-(defmessage update ((game =game=))
+(defmessage update ((game =game=) delta-t)
+  (declare (ignore game delta-t))
   "Takes care of calling UPDATE on all of GAME's member objects. Also, resolves collisions"
-  (with-properties (background player projectiles enemies messages current-frame) game
-    (unless (dead-p player)
-      (resolve-collisions game))
-    (process-cooked-events game)
-    (update background)
-    (mapc #'update projectiles)
-    (update player)
-    (mapc #'update enemies)
-    (mapc #'update messages)))
+)
 
 (defmessage draw ((game =game=))
   (with-properties (background player projectiles enemies messages) game
@@ -60,4 +55,5 @@
     (mapc #'draw projectiles)))
 
 (defmessage resolve-collisions ((game =game=))
+  (declare (ignore game))
   nil)

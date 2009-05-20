2cd7ed3b92e4cb2ac4dee31b7d21059db1e1a04b
Josh <sykopomp@sykosomatic.org>
2009-05-20 17:06:57 -0400

    Added attach/detach messages

--- src/component.lisp
+++ src/component.lisp
@@ -3,5 +3,25 @@
 (defsheep =component= ()
   ((parent nil)
    (initialized-p nil)
+   (visible-p t)
    (subcomponents nil)))
 
+(defmessage update ((component =component=) dt)
+  (values))
+(defmessage draw ((component =component=))
+  (values))
+
+(defmessage attach ((component =component=) (game =game=))
+  (push component (components game)))
+
+(defmessage detach ((component =component=) (game =game=))
+  (setf (components game)
+	(delete component (components game))))
+
+(defmessage attach ((component =component=) (engine =engine=))
+  (let ((game (car (games engine))))
+    (attach component game)))
+
+(defmessage detach ((component =component=) (engine =engine=))
+  (let ((game (car (games engine))))
+    (detach component game)))

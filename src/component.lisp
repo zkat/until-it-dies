c6fe7b986dd61798dd272a6d569f420abf3a8efb
Kat Marchán <kzm@sykosomatic.org>
2009-05-23 03:18:18 -0400

    Adding resources.lisp, other minor updates

--- src/component.lisp
+++ src/component.lisp
@@ -5,27 +5,18 @@
    (subcomponents nil)
    (x 0)
    (y 0)
-   (z 0)
-   (velocity-x 0)
-   (velocity-y 0)
-   (velocity-z 0)))
+   (z 0)))
 
 (defmessage update ((component =component=) dt)
   "By default, UPDATE simply changes the x, y, and z positions of COMPONENT based on its
 velocities for each."
-  #+nil(with-properties (x y z (dx velocity-x) (dy velocity-y) (dz velocity-z))
-      component
-    (incf x (* (/ dt 1000) dx))
-    (incf y (* (/ dt 1000) dy))
-    (incf z (* (/ dt 1000) dz)))
+  (declare (ignore component dt))
   (values))
 
 (defmessage draw ((component =component=))
   "We'll just draw red squares for default =components="
-  (with-accessors ((x x) (y y)) component
-    (gl:color 1 1 1)
-    (gl:with-primitives :quads
-     (rectangle x y 100 100))))
+  (declare (ignore component))
+  (values))
 
 (defmessage attach ((component =component=) (screen =screen=))
   (push component (components screen))
@@ -45,3 +36,11 @@ velocities for each."
   "Ditto here -- they probably just want to attach COMPONENT to ENGINE's first screen."
   (let ((screen (first (screens engine))))
     (detach component screen)))
+
+(defsheep =mobile= (=component=)
+  ((x-velocity 0)
+   (y-velocity 0)
+   (z-velocity 0)
+   (x-rotation 0)
+   (y-rotation 0)
+   (z-rotation 0)))
---
+++ src/resources.lisp
@@ -0,0 +1,6 @@
+(in-package :until-it-dies)
+
+(defsheep =resource= ()
+  ((name "=resource=")
+   (path nil)
+   (data nil)))

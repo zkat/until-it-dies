13ed869eb74a17625e4e15ca2e8a73ea8bca93bb
Kat Marchán <kzm@sykosomatic.org>
2009-05-25 02:18:44 -0400

    Reorganizing code a lot. Working on resources, specifically textures.

--- src/resources.lisp
+++ src/resources.lisp
@@ -1,6 +1,58 @@
 (in-package :until-it-dies)
 
-(defsheep =resource= ()
-  ((name "=resource=")
-   (path nil)
-   (data nil)))
+(defsheep =resource= () ())
+
+(defbuzzword load-texture (texture))
+(defbuzzword bind-texture (texture))
+(defbuzzword unload-texture (texture))
+(defbuzzword reload-texture (texture))
+(defbuzzword loadedp (texture))
+
+(defsheep =texture= ()
+  ((tex-id nil)
+   (target :texture-2d)))
+
+(defmessage bind-texture ((texture =texture=))
+  (unless (tex-id texture)
+    (load-texture texture))
+  (gl:bind-texture (target texture) (tex-id texture)))
+
+(defmessage unload-texture ((texture =texture=))
+  (let* ((id (tex-id texture)))
+    (setf (tex-id texture) nil)
+    (handler-case
+	(gl:delete-textures (list id))
+      #+cl-opengl-checks-errors(%gl::opengl-error (c) (values nil c)))))
+
+(defsheep =file-texture= (=texture=)
+  ((filepath nil)))
+
+(defmessage load-texture ((texture =file-texture=))
+  (when (tex-id texture)
+    (delete-texture texture))
+  (setf (tex-id texture)   
+	(let ((texture-name (car (gl:gen-textures 1)))
+	      (image (sdl-image:load-image (filepath texture)))
+	      (target (target texture)))
+	  (gl:bind-texture target texture-name)
+	  (gl:tex-parameter target :generate-mipmap t)
+	  (gl:tex-parameter target :texture-min-filter :linear-mipmap-linear)
+	  (sdl-base::with-pixel (pix (sdl:fp image))
+	    (let ((texture-format (ecase (sdl-base::pixel-bpp pix)
+				    (1 :luminance)
+				    (2 :luminance-alpha)
+				    (3 :rgb)
+				    (4 :rgba))))
+	      (assert (and (= (sdl-base::pixel-pitch pix)
+			      (* (sdl:width image) (sdl-base::pixel-bpp pix)))
+			   (zerop (rem (sdl-base::pixel-pitch pix) 4))))
+	      (gl:tex-image-2d target 0 :rgba
+			       (sdl:width image) (sdl:height image)
+			       0
+			       texture-format
+			       :unsigned-byte (sdl-base::pixel-data pix))))
+	  texture-name)))
+
+(defmessage loadedp ((texture =texture=))
+  (when (tex-id texture)
+    t))
--- src/util/utils.lisp
+++ src/util/utils.lisp
@@ -1,15 +1,24 @@
 (in-package :until-it-dies)
 
+;;;
+;;; Time
+;;;
 (defun now ()
   (sdl:sdl-get-ticks))
 
 (defun time-difference (time-before)
   "Checks the difference between the internal-time provided and the current time.
 Returns both the difference in time and the current-time used in the computation"
-  (let ((time-now (now)))
-    (values (- time-now time-before)
-	    time-now)))
+  (let* ((time-now (now))
+	 (difference (- time-now time-before)))
+    (if (minusp difference)
+	(time-difference time-now) ; protection for when sdl-get-ticks wraps
+	(values (- time-now time-before)
+		time-now))))
 
+;;;
+;;; Restarting and interactivity
+;;;
 (defmacro restartable (&body body)
   "helper macro since we use continue restarts a lot
  (remember to hit C in slime or pick the restart so errors don't kill the app)"
@@ -17,8 +26,37 @@ Returns both the difference in time and the current-time used in the computation
       (progn ,@body)
     (continue () :report "Continue")))
 
+;;;
+;;; Maths
+;;;
 (defun degrees->radians (degrees)
   (* (/ degrees 180) pi))
 
 (defun radians->degrees (radians)
   (/ (* radians 180) pi))
+
+;;;
+;;; OpengGL utils
+;;;
+(defun setup-ortho-projection (width height)
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (gl:viewport 0 0 width height)
+  (gl:ortho 0 width 0 height 0 100) ;0,0 is at bottom left of screen. Much nicer for maths.
+  (gl:matrix-mode :modelview))
+
+(defun rectangle (x y width height &key (z 0) (u1 0) (v1 0) (u2 1) (v2 1))
+  (let* ((w/2 (/ width 2.0))
+         (h/2 (/ height 2.0))
+         (x1 (- x w/2))
+         (x2 (+ x w/2))
+         (y1 (- y h/2))
+         (y2 (+ y h/2)))
+    (gl:tex-coord u1 v2)
+    (gl:vertex x1 y1 z)
+    (gl:tex-coord u2 v2)
+    (gl:vertex x2 y1 z)
+    (gl:tex-coord u2 v1)
+    (gl:vertex x2 y2 z)
+    (gl:tex-coord u1 v1)
+    (gl:vertex x1 y2 z)))

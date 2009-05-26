e1948323469a6ca4232f252683368942a64ef28d
Josh M <sykopomp@sykosomatic.org>
2009-05-25 23:57:18 -0400

    Improved create-sprite; added DETACH-ALL

--- demo/testgame.lisp
+++ demo/testgame.lisp
@@ -8,22 +8,16 @@
   ((title "Test Engine")
    (screens (list (clone (=screen=) ())))))
 
-(defsheep =test-sprite= (=sprite=)
-  ((x 100)
-   (y 100)
-   (width 100)
-   (height 100)
-   (texture (create-texture
-	     "/home/zkat/hackery/lisp/until-it-dies/res/lisplogo_alien_256.png"))))
-
 (defun create-sprite (filepath &key (x 0) (y 0))
-  (let ((texture (create-texture filepath)))
+  (let* ((texture (load-resource (create-texture filepath)))
+	 (width (width texture))
+	 (height (height texture)))
     (clone (=sprite=)
 	   ((x x)
 	    (y y)
 	    (texture texture)
-	    (width (width texture))
-	    (height (height texture))))))
+	    (width width)
+	    (height height)))))
 
 ;; UID> (attach =test-sprite= =test-engine=)
 ;;

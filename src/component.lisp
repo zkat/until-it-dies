1b7be855a348173e0249c58d0363d7b6fbc75dc1
Kat Marchán <kzm@sykosomatic.org>
2009-05-28 14:29:24 -0400

    More documentation.

--- src/component.lisp
+++ src/component.lisp
@@ -1,5 +1,16 @@
+;; This file is part of Until It Dies
+
+;; component.lisp
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :until-it-dies)
 
+;;;
+;;; Component prototype
+;;;
+;;; - A component is an object that can be drawn onto the screen. Components also accept
+;;;   the INIT, TEARDOWN, UPDATE, DRAW, ATTACH, and DETACH messages, which are usually passed
+;;;   down by the screen and/or engine the component is currently attached to.
 (defsheep =component= ()
   ((parent nil)
    (visiblep t)
@@ -10,17 +21,30 @@
    (width 0)
    (height 0)))
 
+(defmessage init ((component =component=))
+  (declare (ignore component))
+  (values))
+
+(defmessage teardown ((component =component=))
+  (declare (ignore component))
+  (values))
+
 (defmessage update ((component =component=) dt)
   (declare (ignore component dt))
   (values))
 
+(defmessage draw :around ((component =component=))
+  "We wrap the main DRAW message so that components are only drawn 
+   if they are marked as visible."
+  (when (visiblep component)
+  (call-next-message)))
+
 (defmessage draw ((component =component=))
   "We'll just draw a simple rectangle for default =components="
-  (when (visiblep component)
-   (with-properties (x y z width height) 
-       component
-     (gl:with-primitives :quads
-       (rectangle x y width height :z z)))))
+  (with-properties (x y z width height) 
+      component
+    (gl:with-primitives :quads
+      (rectangle x y width height :z z))))
 
 (defmessage attach ((component =component=) (screen =screen=))
   (push component (components screen))
@@ -41,6 +65,11 @@
   (let ((screen (first (screens engine))))
     (detach component screen)))
 
+;;;
+;;; Mobile prototype
+;;;
+;;; - A mobile component is a component with acceleration and velocity. Based on those
+;;;   values, the x, y, and z positions are updated by the UPDATE message.
 (defsheep =mobile= (=component=)
   ((x-velocity 0)
    (y-velocity 0)
@@ -50,8 +79,9 @@
    (z-accel 0)))
 
 (defmessage update ((mobile =mobile=) dt)
-  (with-properties (x y z x-velocity y-velocity z-velocity
-		      x-accel y-accel z-accel) mobile
+  (with-properties (x y z x-accel y-accel z-accel
+		    x-velocity y-velocity z-velocity) 
+      mobile
     ;; First, we adjust the velocity according to current acceleration
     (incf x-velocity (* x-accel dt 1/1000))
     (incf y-velocity (* y-accel dt 1/1000))
@@ -61,15 +91,38 @@
     (incf y (* y-velocity dt 1/1000))
     (incf z (* z-velocity dt 1/1000))))
 
+;;;
+;;; Textured prototype
+;;;
+;;; - Not to be confused with =texture=; =textured= is a component that can have a texture
+;;;   slapped on it. A :before message on =textured= takes care of binding the texture.
 (defsheep =textured= (=component=)
-  ((texture nil)))
+  ((texture =texture=)))
 
 (defmessage draw :before ((component =textured=))
 	    (when (texture component)
 	      (bind-texture (texture component))))
 
+;;;
+;;; Sprite prototype
+;;;
+;;; - Sprites are mobile, textured components that are initialized to be the same size as the
+;;;   texture they are drawn with.
+;;; - TODO: I can't get the width/height of the texture until it gets loaded. Find a way to do it.
 (defsheep =sprite= (=mobile= =textured=)
   ())
+
+#+nil(defmessage initialize-sheep :after ((sprite =sprite=) &key)
+  (with-properties (width height texture)
+      sprite
+    (setf width (width texture))
+    (setf height (height texture))))
+
+#+nil(defmessage reinitialize-sheep :after ((sprite =sprite=) &key)
+  (with-properties (width height texture)
+      sprite
+    (setf width (width texture))
+    (setf height (height texture))))
 
 (defmessage draw ((sprite =sprite=))
   (bind-texture (texture sprite))

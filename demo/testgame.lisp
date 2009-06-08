d23ee26870d56c8114b4367475d0dd8028f6080c
Josh <sykopomp@sykosomatic.org>
2009-06-08 02:21:32 -0500

    New API.

--- demo/testgame.lisp
+++ demo/testgame.lisp
@@ -4,44 +4,35 @@
 
 (defsheep =test-engine= (uid::=engine=)
   ((title "Test Engine")
-   (screens (list (clone (uid::=screen=) ())))
    (window-width 600)
    (window-height 600)))
 
 (defparameter *test-image*
   (uid::create-image
-   "/home/zkat/hackery/lisp/until-it-dies/res/lisplogo_alien_256.png"
-   :x 50
-   :y 50))
+   "/home/zkat/hackery/lisp/until-it-dies/res/lisplogo_alien_256.png"))
 
+(defparameter *x* 50)
+(defparameter *y* 50)
 (defparameter *speed* 200)
 
 (defmessage uid::update ((engine =test-engine=) dt)
   (declare (ignore engine))
   (when (uid::key-down-p :right)
-    (incf (uid::x *test-image*) (* *speed* dt)))
+    (incf *x* (* *speed* dt)))
   (when (uid::key-down-p :left)
-    (decf (uid::x *test-image*) (* *speed* dt)))
+    (decf *x* (* *speed* dt)))
   (when (uid::key-down-p :up)
-    (incf (uid::y *test-image*) (* *speed* dt)))
+    (incf *y* (* *speed* dt)))
   (when (uid::key-down-p :down)
-    (decf (uid::y *test-image*) (* *speed* dt))))
+    (decf *y* (* *speed* dt))))
 
 (defmessage uid::draw ((engine =test-engine=))
-  (draw :rectangle 100 100))
-;; (uid::attach *test-image* =test-engine=)
-
-;; VS
-
-;; (defmessage uid::draw ((engine =test-engine=))
-;;   (declare (ignore engine))
-;;   (uid::draw *test-image*))
-
-;; (defmessage uid::init ((engine =test-engine=))
-;;   (declare (ignore engine))
-;;   (uid::init *test-image*)
-;;   (call-next-message))
-
-(defbuzzword draw (foo x y))
-(defmessage draw ((obj :rectangle) x y)
-  (uid::draw-rectangle x y 20 20))
+  (declare (ignore engine))
+  #+nil(gl:with-pushed-matrix
+    (gl:translate 50 50 0)
+    (ftgl:render-font (uid::font-pointer uid::*font*) "foobar" :all))
+  (dotimes (i 500)
+   (uid::draw-sprite "foo" 
+                     (random 600)
+                     (random 600)))
+  (uid::draw-sprite *test-image* *x* *y*))

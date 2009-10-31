326c765ff470e9b9555b980505363af507da830b
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-30 23:52:12 -0500

    Hopeful fix for pathname shenanigans.

--- src/config.lisp
+++ src/config.lisp
@@ -7,15 +7,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :until-it-dies)
 
-(defparameter *resource-path-name* "resources/")
-(defparameter *level-path-name* "levels/")
+(defparameter *resource-path-name* "res/")
 (defun get-resource-path ()
-  (merge-pathnames *resource-path-name*
-                   #+ccl(concatenate 'string (ccl::current-directory-name) "/")
-                   #+sbcl(values *default-pathname-defaults*)
-                   #+clisp(ext:default-directory)))
-(defun get-level-path ()
-  (merge-pathnames *level-path-name*
-                   #+clisp(ext:default-directory)
-                   #+sbcl(values *default-pathname-defaults*)
-                   #+ccl(concatenate 'string (ccl::current-directory-name) "/")))
+  (merge-pathnames *resource-path-name* (truename "./")))
+

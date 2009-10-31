c56b1fd9768c084c1864fe59ad7e582f2b9a2085
Josh Marchán <sykopomp@sykosomatic.org>
2009-10-31 14:02:25 -0500

    Moved fonts into their own file.

--- src/resources/resources.lisp
+++ src/resources/resources.lisp
@@ -72,62 +72,6 @@ variable within its scope."))
   (detach resource *resource-manager*))
 
 ;;;
-;;; Fonts
-;;;
-(defproto =font= (=file-resource=)
-  ((font-pointer nil)
-   (size 12)
-   (res 100)
-   (loadedp nil)
-   (filepath (truename "res/example.otf")))
-  (:documentation "A font is used by the text-drawing system to draw strings to screen."))
-
-(defun create-font (filepath &key (size 12) (res 20))
-  (defobject (=font=) ((filepath filepath) (size size) (res res))))
-
-(defvar *font* =font=)
-
-(defmacro with-font (font &body body)
-  "Binds *default-font* to FONT within BODY."
-  `(let ((*font* ,font))
-     ,@body))
-
-(defreply load-resource :before ((font =font=))
-  (when (font-pointer font)
-    (unload-resource font)))
-
-(defreply load-resource ((font =font=))
-  (setf (font-pointer font)
-        (ftgl:create-texture-font (namestring (filepath font))))
-  (ftgl:set-font-face-size (font-pointer font)
-                           (size font)
-                           (res font))
-  (setf (loadedp font) t)
-  font)
-
-(defreply load-resource :after ((font =font=))
-  (let ((ptr (font-pointer font)))
-    (finalize font (lambda ()
-                     (ftgl:destroy-font ptr)))))
-
-(defreply unload-resource ((font =font=))
-  (ftgl:destroy-font (font-pointer font))
-  (setf (font-pointer font) nil)
-  (setf (loadedp font) nil)
-  font)
-
-;; Anytime we change a font's dimensions while *engine* is initialized, we should reload it.
-(defreply (setf size) :after (new-size (font =font=))
-  (declare (ignore new-size))
-  (when (initializedp *engine*)
-    (load-resource font)))
-
-(defreply (setf res) :after (new-res (font =font=))
-  (declare (ignore new-res))
-  (when (initializedp *engine*)
-    (load-resource font)))
-
-;;;
 ;;; Sound
 ;;;
 ;; TODO - check for errors when loading/unloading/playing

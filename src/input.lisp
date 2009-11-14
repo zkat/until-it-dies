d716110bfd4d3203f207ad39971ed52641e11a6b
Josh Marchán <sykopomp@sykosomatic.org>
2009-11-13 23:01:49 -0600

    Adding basics of joystick support.

--- src/input.lisp
+++ src/input.lisp
@@ -74,3 +74,48 @@
 
 (defun translate-glfw-control-key (keycode)
   (svref *glfw-control-keys* (glfw-vector-index keycode)))
+
+;;;
+;;; Joystick support
+;;;
+
+;;; low-level stuff
+(defun glfw-available-joysticks ()
+  (loop for i below 16 
+     for joystick-present-p = (uid-glfw:get-joystick-param i uid-glfw:+present+)
+     when (= 1 joystick-present-p)
+     collect i))
+
+(defun glfw-joystick-num-buttons (joystick-number)
+  (uid-glfw:get-joystick-param joystick-number uid-glfw:+buttons+))
+
+(defun glfw-joystick-num-axes (joystick-number)
+  (uid-glfw:get-joystick-param joystick-number uid-glfw:+axes+))
+
+(defun glfw-joystick-axis-positions (joystick-number num-axes)
+  (uid-glfw:get-joystick-pos joystick-number num-axes))
+
+(defun glfw-joystick-button-states (joystick-number num-buttons)
+  (loop for state in (uid-glfw:get-joystick-buttons joystick-number num-buttons)
+     collect (if (= uid-glfw:+press+) :pressed :released)))
+
+;;; Interface
+(defproto =joystick= ()
+  (joystick-number 0)
+  (num-axes 0) (num-buttons 0)
+  (axis-positions nil) (button-states nil))
+
+(defun build-joystick (joynum)
+  (let ((num-axes (glfw-joystick-num-axes))
+        (num-buttons (glfw-joystick-num-buttons)))
+    (defobject =joystick=
+        ((joystick-number joynum)
+         (num-axes num-axes)
+         (num-buttons num-buttons)
+         (axis-positions (glfw-joystick-axis-positions joynum num-axes))
+         (button-states (glfw-joystick-button-states joynum num-buttons))))))
+
+(defun available-joysticks ()
+  (loop for joystick-num in (glfw-available-joysticks)
+     collect (build-joystick joystick-num)))
+

94badfc35ab110c36b4deb3082ffab090ac13a51
Kat Marchán <kzm@sykosomatic.org>
2009-12-26 17:32:35 -0600

    DEMO: The demo no longer imports the UID package.
    While this makes the demo much noisier, it's useful for showing users which parts of the demo are
    specifically UID's, and which are user code. It's possible that -some- users would be willing to
    prepend all these symbols with uid:, but it's not necessary to do so.

--- demo/testgame.lisp
+++ demo/testgame.lisp
@@ -1,5 +1,5 @@
 (defpackage uid-demo
-  (:use :cl :sheeple :until-it-dies)
+  (:use :cl :sheeple)
   (:shadow :speed)
   (:export :run-demo))
 (in-package :uid-demo)
@@ -18,30 +18,30 @@
    (y 0)
    content))
 
-(defreply draw ((thing =game-object=) &rest args &key)
+(defreply uid:draw ((thing =game-object=) &rest args &key)
   (with-properties (x y content) thing
-    (apply 'draw-at x y content args)))
+    (apply 'uid:draw-at x y content args)))
 
-(defreply update ((thing =game-object=) dt &rest args &key)
-  (apply 'update (content thing) dt args))
+(defreply uid:update ((thing =game-object=) dt &rest args &key)
+  (apply 'uid:update (content thing) dt args))
 
 ;; We use defproto here for convenience, but keep our *earmuffs*
 (defproto *alien* =game-object=
-  ((content (create-image (merge-pathnames "lisplogo_alien_256.png" *resource-directory*)))
+  ((content (uid:create-image (merge-pathnames "lisplogo_alien_256.png" *resource-directory*)))
    visiblep (x 255) (y 356)))
 
-(defparameter *bah* (create-sound (merge-pathnames "sample.wav" *resource-directory*)))
+(defparameter *bah* (uid:create-sound (merge-pathnames "sample.wav" *resource-directory*)))
 
-(defreply draw :around ((thing *alien*) &key)
+(defreply uid:draw :around ((thing *alien*) &key)
   (with-properties (visiblep) thing
     (when visiblep (call-next-reply))))
 
 (defproto *anim* =game-object=
-  ((content (create-animation (merge-pathnames "explosion.png" *resource-directory*) 15 14 0.05 14))
+  ((content (uid:create-animation (merge-pathnames "explosion.png" *resource-directory*) 15 14 0.05 14))
    (speed 300) (x 50) (y 50)))
 
-(defproto *our-font* =font=
-  ((filepath (merge-pathnames "example.otf" *resource-directory*))))
+(defproto *our-font* uid:=font=
+  ((uid:filepath (merge-pathnames "example.otf" *resource-directory*))))
 
 (defproto *circle* =game-object=
   ((x 100) (y 100) (dx/dt 0.0) (dy/dt 0.0)
@@ -56,26 +56,26 @@
   ;; The following assumes that angle has been produced by `cl:atan', and
   ;; depends heavily upon that and the gory details of the function.
   (flet ((ndist (a b) (abs (- a b))))
-    (make-color :r (- 1 (/ (ndist angle 0) pi 2/3))
-                :g (- 1 (/ (ndist (mod angle (* 2 pi)) (* pi 2/3)) pi 2/3))
-                :b (- 1 (/ (ndist (mod angle (* 2 pi)) (* pi 4/3)) pi 2/3)))))
+    (uid:make-color :r (- 1 (/ (ndist angle 0) pi 2/3))
+                    :g (- 1 (/ (ndist (mod angle (* 2 pi)) (* pi 2/3)) pi 2/3))
+                    :b (- 1 (/ (ndist (mod angle (* 2 pi)) (* pi 4/3)) pi 2/3)))))
 
-(defreply draw ((thing *circle*) &key)
+(defreply uid:draw ((thing *circle*) &key)
   (with-properties (x y radius dx/dt dy/dt) thing
     (flet ((ndist (a b) (abs (- a b))))
       (let* ((speed (sqrt (+ (* dx/dt dx/dt) (* dy/dt dy/dt))))
              (brightness (max (/ speed 4) 0.3))
              (color (angle->color (atan dy/dt dx/dt))))
-        (with-properties (red green blue) color
-          (setf red (* red brightness)
-                green (* green brightness)
-                blue (* blue brightness)))
-        (with-color color
-          (draw-circle (make-point x y) radius :filledp t))))))
+        (with-properties (uid:red uid:green uid:blue) color
+          (setf uid:red (* uid:red brightness)
+                uid:green (* uid:green brightness)
+                uid:blue (* uid:blue brightness)))
+        (uid:with-color color
+          (uid:draw-circle (uid:make-point x y) radius :filledp t))))))
 
-(defreply update ((thing *circle*) dt &key)
+(defreply uid:update ((thing *circle*) dt &key)
   (declare (ignore dt))
-  (with-properties ((x mouse-x) (y mouse-y)) *uid-demo*
+  (with-properties ((x uid:mouse-x) (y uid:mouse-y)) =uid-demo=
     (with-properties (dx/dt dy/dt (cx x) (cy y)) *circle*
       (macrolet ((apply-velocity (coordinate velocity bound)
                    `(progn (when (> (abs ,velocity) 5)
@@ -84,58 +84,58 @@
                              (setf ,velocity (- ,velocity)
                                    ,coordinate (if (plusp ,coordinate) ,bound 0)))
                            (incf ,coordinate ,velocity))))
-        (apply-velocity cx dx/dt (window-width *uid-demo*))
-        (apply-velocity cy dy/dt (window-height *uid-demo*)))
+        (apply-velocity cx dx/dt (uid:window-width =uid-demo=))
+        (apply-velocity cy dy/dt (uid:window-height =uid-demo=)))
       ;; This is _basically_ Newton's Law of Universal Gravitation
       (let* ((x-gap (- x cx)) (y-gap (- y cy))
              (accel (/ (+ (expt x-gap 2) (expt y-gap 2)))))
         (incf dx/dt (* accel x-gap))
         (incf dy/dt (* accel y-gap))))))
 
-(defreply update ((engine *uid-demo*) dt &key)
-  (update *anim* dt)
-  (update *circle* dt)
+(defreply uid:update ((engine =uid-demo=) dt &key)
+  (uid:update *anim* dt)
+  (uid:update *circle* dt)
   (with-properties (x y speed) *anim*
-    (when (and (key-down-p :right)
-               (< x (window-width engine)))
+    (when (and (uid:key-down-p :right)
+               (< x (uid:window-width engine)))
       (incf x (* speed dt)))
-    (when (and (key-down-p :left)
+    (when (and (uid:key-down-p :left)
                (< 0 x))
       (decf x (* speed dt)))
-    (when (and (key-down-p :up)
-               (< y (window-height engine)))
+    (when (and (uid:key-down-p :up)
+               (< y (uid:window-height engine)))
       (incf y (* speed dt)))
-    (when (and (key-down-p :down)
+    (when (and (uid:key-down-p :down)
                (< 0 y))
       (decf y (* speed dt)))))
 
-(defreply draw ((engine *uid-demo*) &key)
+(defreply uid:draw ((engine =uid-demo=) &key)
   (let ((scale-factor 4))
-    (with-color (mix-colors *white* *black* *blue* *green*)
-      (draw-points (loop for i below 100 collect (make-point (random 600) (random 600)))))
-    (with-font *our-font*
-      (draw-at 60 20 "Try left-clicking, right-clicking, and pressing the arrow keys!"))
-    (draw *anim* :x-scale scale-factor :y-scale scale-factor)
-    (draw *circle*)
-    (draw *alien* :rotation 45)))
+    (uid:with-color (uid:mix-colors uid:*white* uid:*black* uid:*blue* uid:*green*)
+      (uid:draw-points (loop for i below 100 collect (uid:make-point (random 600) (random 600)))))
+    (uid:with-font *our-font*
+      (uid:draw-at 60 20 "Try left-clicking, right-clicking, and pressing the arrow keys!"))
+    (uid:draw *anim* :x-scale scale-factor :y-scale scale-factor)
+    (uid:draw *circle*)
+    (uid:draw *alien* :rotation 45)))
 
-(defreply mouse-move :after ((engine *uid-demo*) x y)
+(defreply uid:mouse-move :after ((engine =uid-demo=) x y)
   (with-properties ((alien-x x) (alien-y y)) *alien*
     (setf alien-x x alien-y y)))
 
-(defreply mouse-down ((engine *uid-demo*) button)
+(defreply uid:mouse-down ((engine =uid-demo=) button)
   (with-properties (visiblep) *alien*
     (case button
       (0 (setf visiblep (not visiblep)))
-      (1 (play *bah*)))))
+      (1 (uid:play *bah*)))))
 
-(defreply joystick-button-down ((engine *uid-demo*) joystick button)
-  (when (= 0 (joystick-number joystick))
+(defreply uid:joystick-button-down ((engine =uid-demo=) joystick button)
+  (when (= 0 (uid:joystick-number joystick))
     (case button
-      (0 (play *bah*)))))
+      (0 (uid:play *bah*)))))
 
-(defreply joystick-move ((engine *uid-demo*) joystick axis state)
+(defreply uid:joystick-move ((engine =uid-demo=) joystick axis state)
   (format t "Joystick: ~A Axis: ~A Position: ~A~%" joystick axis state))
 
 (defun run-demo ()
-  (run *uid-demo*))
+  (uid:run =uid-demo=))

8dcd676443372604755158e219ba376486aedd79
Josh Marchán <sykopomp@sykosomatic.org>
2010-06-13 17:09:45 -0500

    Tweaking text demo

--- examples/text.lisp
+++ examples/text.lisp
@@ -18,20 +18,22 @@
 (defmethod uid:on-draw ((window text-window))
   (uid:set-gl-window window)
   (gl:clear-color 1 1 1 1)
-  (gl:clear :color-buffer-bit :depth-buffer-bit :stencil-buffer-bit)
+  (gl:clear :color-buffer-bit :depth-buffer-bit)
 
   (uid:with-color uid:*black*
    (uid:with-font *our-font*
 
-     (uid:draw-at 40 20 (make-instance 'uid:text :string "This is text")
-                       :width 400 :height 400 :align :middle :valign :top)
+     (uid:draw-at 300 400 (make-instance 'uid:text :string "This is text")
+                  :align :middle :valign :bottom)
 
-     (uid:draw "Testing 1 2 3" :x 300 :y 400)))
+     (uid:draw "Testing: 1, 2, 3" :x 300 :y 400 :x-scale 2 :y-scale 2)))
 
   (uid:swap-buffers window))
 
 (defparameter *engine* (make-instance 'text-engine))
 
 (defun run ()
-  (setf (uid:windows *engine*) (list (make-instance 'text-window)))
+  (setf (uid:windows *engine*) (list (make-instance 'text-window
+                                                    :width 200
+                                                    :height 200)))
   (uid:run *engine*))

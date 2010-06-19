858086031a783852b2eaf45abea0886291c3f6f6
Kat Marchán <kzm@sykosomatic.org>
2010-06-19 14:17:55 -0500

    modified text rendering example

--- examples/text.lisp
+++ examples/text.lisp
@@ -13,7 +13,11 @@
 
 (defparameter *our-font* (make-instance 'uid:font
                                         :filepath (merge-pathnames "example.ttf" *resource-directory*)
-                                        :size 20))
+                                        :size 14))
+
+(defparameter *string-to-draw*
+  (make-instance 'uid:text :string
+                 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
 
 (defmethod uid:on-draw ((window text-window))
   (uid:set-gl-window window)
@@ -23,17 +27,18 @@
   (uid:with-color uid:*black*
    (uid:with-font *our-font*
 
-     (uid:draw-at 300 400 (make-instance 'uid:text :string "This is text")
-                  :align :middle :valign :bottom)
+     (uid:draw "Some text: " :x 150 :y 350)
 
-     (uid:draw "Testing: 1, 2, 3" :x 300 :y 400 :x-scale 2 :y-scale 2)))
-
+     (uid:draw-at 10 50 *string-to-draw*
+                  :align :left :valign :middle
+                  :width 400 :height 400)))
+  
   (uid:swap-buffers window))
 
 (defparameter *engine* (make-instance 'text-engine))
 
 (defun run ()
   (setf (uid:windows *engine*) (list (make-instance 'text-window
-                                                    :width 200
-                                                    :height 200)))
+                                                    :width 400
+                                                    :height 400)))
   (uid:run *engine*))

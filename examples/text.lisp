d0531cb7483914430c328bad68850ac7ca43dff2
Kat Marchán <kzm@sykosomatic.org>
2010-06-29 22:10:29 -0500

    text example now uses ftgl font

--- examples/text.lisp
+++ examples/text.lisp
@@ -13,13 +13,12 @@
 
 (defparameter *game* (make-instance 'text-example))
 
-(defparameter *our-font* (make-instance 'uid:font
+(defparameter *our-font* (make-instance 'uid::ftgl-font
                                         :filepath (merge-pathnames "example.ttf" *resource-directory*)
                                         :size 14))
 
 (defparameter *string-to-draw*
-  (make-instance 'uid:text :string
-                 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
 (defmethod uid:on-draw ((game text-example))
   (uid:clear game)
@@ -32,9 +31,7 @@
 
       (uid:draw "Some text: " :x 150 :y 350)
 
-      (uid:draw-at 10 50 *string-to-draw*
-                   :align :left :valign :middle
-                   :width 400 :height 400))))
+      (uid:draw-at 20 300 *string-to-draw*))))
 
 (defun run ()
   (uid:run *game*))
